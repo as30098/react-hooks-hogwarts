14ca80c038f2770405b6ee38dfd649c0550b7e6b
as30098 <alexspencer421@gmail.com>
2021-02-04 13:31:14 -0500

    code with notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
+import HogList from "./HogList"
+import Filter from "./Filter"
 
 import hogs from "../porkers_data";
 
+
 function App() {
+  const [showGreased, setShowGreased] = useState(false);
+  const [sortBy, setSortBy] = useState("name");
+
+
+
+  const hogsToDisplay = hogs
+  // why is the second statement in the ternary "true"????
+    .filter((hog) => (showGreased ? hog.greased : true))
+    // why do we put in two different hogs? Why are they named 1 and 2?
+    .sort((hog1, hog2) => {
+      // where does sortBy come from?
+      // first condition handles the weight
+      if (sortBy === "weight") {
+        return hog1.weight - hog2.weight;
+      } else {
+        // second condition handles the name
+        // need to understand .localeCompare method?? 0/1 values??
+        return hog1.name.localeCompare(hog2.name);
+      }
+    });
+
   return (
-    <div className="App">
-      <Nav />
+    <div className="ui grid container App">
+      <div className="sixteen wide column centered">
+        <Nav />
+      </div>
+      <div className="sixteen wide column centered">
+        <Filter
+        // what is this
+          showGreased={showGreased}
+          onChangeShowGreased={setShowGreased}
+          // what is this
+          sortBy={sortBy}
+          onChangeSortBy={setSortBy}
+        />
+      </div>
+      <div className="sixteen wide column centered">
+        <HogList hogs={hogsToDisplay} />
+      </div>
     </div>
   );
 }
 
+
 export default App;
+
+
---
+++ src/components/HogCard.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import HogDetails from "./HogDetails";
+
+import augustus_gloop from "../assets/augustus_gloop.jpg";
+import babe from "../assets/babe.jpg";
+import bailey from "../assets/bailey.jpg";
+import cherub from "../assets/cherub.jpg";
+import galaxy_note from "../assets/galaxy_note.jpg";
+import leggo_my_eggo from "../assets/leggo_my_eggo.jpg";
+import peppa from "../assets/peppa.jpg";
+import piggy_smalls from "../assets/piggy_smalls.jpg";
+import piglet from "../assets/piglet.jpg";
+import porkchop from "../assets/porkchop.jpg";
+import trouble from "../assets/trouble.jpg";
+import truffle_shuffle from "../assets/truffle_shuffle.jpg";
+
+const images = {
+  "Augustus Gloop": augustus_gloop,
+  Babe: babe,
+  Bailey: bailey,
+  Cherub: cherub,
+  "Galaxy Note": galaxy_note,
+  "Leggo My Eggo": leggo_my_eggo,
+  Peppa: peppa,
+  "Piggy smalls": piggy_smalls,
+  Piglet: piglet,
+  Porkchop: porkchop,
+  Trouble: trouble,
+  "Truffle Shuffle": truffle_shuffle,
+};
+
+function HogCard({ hog }) {
+  const [isHidden, setIsHidden] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
+
+  function handleDetailsClick() {
+    setShowDetails((prevShowDetails) => !prevShowDetails);
+  }
+
+  // hide the hog
+  if (isHidden) return null;
+
+  // show the hog
+  const { name, specialty } = hog;
+
+  return (
+    <div className="ui card eight wide column pigTile">
+      <div className="image">
+        <img src={images[name]} alt="hogPic" />
+      </div>
+      <div className="content">
+        <h3 className="header">{name}</h3>
+        <div className="description">Specialty: {specialty}</div>
+      </div>
+      <div className="extra content">
+        {showDetails ? <HogDetails hog={hog} /> : null}
+        {/* Show info and Hide info button */}
+        <button className="ui button" onClick={handleDetailsClick}>
+          {showDetails ? "Less Info" : "More Info"}
+        </button>
+        {/* Hide Card button */}
+        <button className="ui button" onClick={() => setIsHidden(true)}>
+          Hide Me{" "}
+          <span role="img" aria-label="snout">
+            🐽
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default HogCard;
